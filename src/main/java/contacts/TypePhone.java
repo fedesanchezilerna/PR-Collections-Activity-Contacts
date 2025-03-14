297389a875afad5c472b745659bfbf9c34a3e0d6
Federico Sánchez <fedesanchezvidarte@gmail.com>
2025-03-14 08:44:26 +0100

    Adding TypePhone enum class

--- src/main/java/contacts/TypePhone.java
+++ src/main/java/contacts/TypePhone.java
@@ -1,4 +1,18 @@
 package contacts;
 
 public enum TypePhone {
+    H("Home"),
+    M("Mobile"),
+    THE("Office"),
+    F("Fax Office");
+
+    private final String text;
+
+    TypePhone(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
